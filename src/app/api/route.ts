a0796f711928b7f54ecd4a7d6fca2a7a8f436755
Oleg Korol <oleg@olegkorol.de>
2025-02-19 18:10:58 +0400

    remove experiments for now

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,13 +1,9 @@
-import { kv } from "../../../denoDeploy.ts";
-
-export async function GET() {
-  const name = await kv.get(["name"]);  
-  return Response.json(`Hello from Deno Deploy, ${name}!`)
+export function GET() {
+  return Response.json("Hello from Deno Deploy!")
 }
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { name } = body;
-  await kv.set(["name"], name);
-  return Response.json(`Saved your name, ${name}!`)
+  return Response.json(`Hello from Deno Deploy, ${name ?? "stranger"}!`)
 }
