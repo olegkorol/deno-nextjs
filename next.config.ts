abb6dc5470a610a4e1f0905887c536bbe1a086d6
Oleg Korol <oleg@olegkorol.de>
2025-02-19 14:18:27 +0400

    an even more interesting experiment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    // Add node-loader for .node files (this is needed for Deno KV to work)
+    config.externals = [...(config.externals || []), /\.node$/]
     config.module.rules.push({
       test: /\.node$/,
       use: 'node-loader'
