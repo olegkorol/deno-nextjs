af715696c95fd61547e10a8b30adc6169f2c3a8c
Oleg Korol <oleg@olegkorol.de>
2025-02-19 13:40:36 +0400

    get/post routes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,8 @@
- 
-export function GET() { 
+export function GET() {
+  return Response.json("Hello from Deno Deploy!")
+}
+
+export function POST(request: Request) {
+  console.log(request.body)
   return Response.json("Hello from Deno Deploy!")
 }
